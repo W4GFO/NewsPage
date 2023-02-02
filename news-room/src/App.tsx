780f4f2e16d94c19f021e1ce35bdd464722dbe20
Jeff Behrbaum <sejapresident@gmail.com>
2023-02-02 10:57:21 -0500

    Initial commit of dev code

---
+++ news-room/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import {HomePage} from './Components/HomePage'
+
+function App() {
+  return (
+    <main>
+      <HomePage />
+    </main>
+  );
+}
+
+export default App;
---
+++ news-room/src/Components/HomePage.tsx
@@ -0,0 +1,9 @@
+import './Styles/HomePageStyle.scss'
+
+type Props = {}
+
+export const HomePage = (props: Props) => {
+	return (
+		<></>
+	)
+}
